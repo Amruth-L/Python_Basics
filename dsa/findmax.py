39ff8e0af44f29045b8975eaa8d6a4178698a0a3
Amruth-L <amruthln21@gmail.com>
2026-08-17 20:28:20 +0530

    added

---
+++ dsa/findmax.py
@@ -0,0 +1,6 @@
+arr=[4,7,2,9,1]
+max_value=arr[0]
+for x in arr:
+    if x>max_value:
+        max_value=x
+print(max_value)
--- dsa/twosum.py
+++ dsa/twosum.py
@@ -1,9 +1,18 @@
-nums=list(map(int,input("Enter the numbers : ").split(",")))
-target=int(input("enter the target number : "))
+# nums=list(map(int,input("Enter the numbers : ").split(",")))
+# target=int(input("enter the target number : "))
+# def twosum(nums,target):
+#     n=len(nums)
+#     for i in range(n):
+#         for j in range(i+1,n):
+#             if nums[i]+nums[j]==target:
+#                 return [i,j]
+# print (twosum(nums,target))
 def twosum(nums,target):
-    n=len(nums)
-    for i in range(n):
-        for j in range(i+1,n):
-            if nums[i]+nums[j]==target:
-                return [i,j]
-print (twosum(nums,target))
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return[i,j]
+nums=[2,7,11,15]
+target=9
+result=twosum(nums,target)
+print(result)
