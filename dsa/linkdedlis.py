6d2ad2518ddb4a04e988ab4ca388f502f79d8223
Amruth-L <amruthln21@gmail.com>
2026-09-20 23:08:32 +0530

    reverselist

---
+++ dsa/linkdedlis.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# Create nodes
+head = Node(10)
+
+head.next = Node(20)
+head.next.next = Node(30)
+head.next.next.next = Node(40)
+
+
+# Reverse the linked list
+prev = None
+current = head
+
+while current:
+    next_node = current.next
+    current.next = prev
+    prev = current
+    current = next_node
+
+head = prev
+
+
+# Print the reversed list
+current = head
+
+while current:
+    print(current.data, end=" -> ")
+    current = current.next
+
+print("None")
